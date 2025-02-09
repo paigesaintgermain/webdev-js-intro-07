c79c5a7b25df279186f15d7cf93749b7e4014c74
Paige Saint Germain <paigejerin@gmail.com>
2025-02-09 13:12:26 -0500

    completed assignment 07

--- js/assignment.js
+++ js/assignment.js
@@ -1,8 +1,37 @@
-"use strict";
+'use strict';
 
-const submissionBtn = document.getElementById("submission-btn");
-const remainderElement = document.getElementById("remainder");
-const checkIfEvenElement = document.getElementById("check-if-even");
-const lostNumbersElement = document.getElementById("lost-numbers");
+const submissionBtn = document.getElementById('submission-btn');
+const remainderElement = document.getElementById('remainder');
+const checkIfEvenElement = document.getElementById('check-if-even');
+const lostNumbersElement = document.getElementById('lost-numbers');
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+function returnTheRemainder(a, b) {
+    return a % b;
+}
+
+const checkIfEven = (number) => {
+    // I could save myself the ELSE statement if I set this and just change it if true.
+    let isEven = false;
+
+    if (number % 2 === 0) {
+        isEven = true;
+    }
+
+    return isEven;
+};
+
+const getTheFourthElement = () => {
+    return lostNumbers[3];
+};
+
+const render = () => {
+    remainderElement.innerHTML = returnTheRemainder(5, 4);
+    checkIfEvenElement.innerHTML = checkIfEven(47);
+    lostNumbersElement.innerHTML = getTheFourthElement();
+};
+
+submissionBtn.addEventListener('click', function () {
+    render();
+});
--- js/index.js
+++ js/index.js
@@ -1,4 +1,52 @@
-// Prevent us from attempting to use variables
-// that are not declared
-"use strict"
+// // Prevent us from attempting to use variables
+// // that are not declared
+// 'use strict';
+
+// //  Create some functions return a value and log that value to the console.
+// function never() {
+//     return 'Never';
+// }
+
+// function gonna() {
+//     return 'gonna';
+// }
+
+// function give() {
+//     return 'give';
+// }
+
+// function you() {
+//     return 'you';
+// }
+
+// function up() {
+//     return 'up';
+// }
+
+// console.log(`${never()} ${gonna()} ${give()} ${you()} ${up()}.`);
+
+// //  Create some functions that take in arguments. Try to write at one least one that returns a value and one that doesn't.
+// const gaveYouUp = (gaveUp) => {
+//     if (gaveUp == true) {
+//         console.log('Actually...I give up.');
+//     }
+// };
+
+// const letYouDown = (letDown) => {
+//     if (letDown != true) {
+//         return 'Never gonna let you down.';
+//     }
+// };
+
+// gaveYouUp(false);
+// console.log(letYouDown(false));
+
+// //  Create some functions that take in arguments, but also have default parameters.
+// const desertYou = (ranAround, deserted = false) => {
+//     if (ranAround == false && deserted == false) {
+//         console.log('Never gonna run around and desert you.');
+//     }
+// };
+
+// desertYou(false);
 
